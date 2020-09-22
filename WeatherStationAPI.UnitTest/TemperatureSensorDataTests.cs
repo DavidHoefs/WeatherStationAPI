b77d86ccc589633113a80d84e941c31550aa544c
David Hoefs <david.hoefs@ttu.edu>
2020-09-22 15:32:45 -0500

    Unit Tests Added
    Not fully functional yet

---
+++ WeatherStationAPI.UnitTest/TemperatureSensorDataTests.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac.Extras.Moq;
+using Autofac;
+using WeatherStationAPI.Library.Internal.DataAccess;
+using WeatherStationAPI.Library.Models;
+using Xunit;
+using WeatherStationAPI.Library.DataAccess;
+using WeatherStationAPI.Controllers;
+using Moq;
+
+namespace WeatherStationAPI.UnitTest
+{
+    public class TemperatureSensorDataTests
+    {
+        [Fact]
+        public void GetData_ValidCall()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<ISqlDataAccess>()
+                    .Setup(x => x.LoadData<TemperatureSensorModel, dynamic>("dbo.spTemperatureSensor_GetData", new { }, "WeatherStationDB"))
+                    .Returns(GetSampleTemps());
+
+                var controller = mock.Create<TemperatureSensorData>();
+                var expected = GetSampleTemps();
+                var actual = controller.GetData();
+
+                Assert.True(actual != null);
+                Assert.Equal(expected.Count, actual.Count);
+
+                for(int i = 0; i< expected.Count; i++)
+                {
+                    Assert.Equal(expected[i].Temperature, actual[i].Temperature);
+                }
+
+            }
+           
+        }
+
+        [Fact]
+        public void SaveSensorData_ValidCall()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                var temp = GetSampleDBTemps()[0];
+                mock.Mock<ISqlDataAccess>()
+                    .Setup(x => x.SaveData("dbo.spTemperatureSensor_UploadData", temp, "WeatherStationDB"));
+
+                var cls = mock.Create<TemperatureSensorController>();
+               
+
+                cls.Post(temp);
+
+                mock.Mock<ISqlDataAccess>()
+                   .Verify(x => x.SaveData("dbo.spTemperatureSensor_GetData", temp, "WeatherStationDB"), Times.Exactly(1));
+
+
+
+
+
+
+
+            }
+        }
+        private List<TemperatureSensorDBModel> GetSampleDBTemps()
+        {
+            List<TemperatureSensorDBModel> output = new List<TemperatureSensorDBModel>
+            {
+                new TemperatureSensorDBModel
+                {
+                    Temperature = 77,
+                    Humidity = 0.22,
+                    TimeCaptured = DateTime.Parse("2020-09-21T20:00:00")
+                },
+                new TemperatureSensorDBModel
+                {
+                    Temperature = 77,
+                    Humidity = 0.22,
+                    TimeCaptured = DateTime.Parse("2020-09-21T20:00:00")
+                },
+                new TemperatureSensorDBModel
+                {
+                    Temperature = 77,
+                    Humidity = 0.22,
+                    TimeCaptured = DateTime.Parse("2020-09-21T20:00:00")
+                }
+            };
+            return output;
+        }
+        private List<TemperatureSensorModel> GetSampleTemps()
+        {
+            List<TemperatureSensorModel> output = new List<TemperatureSensorModel>
+            {
+                new TemperatureSensorModel
+                {
+                    Temperature = 77,
+                    Humidity = 0.22,
+                    TimeCaptured = DateTime.Parse("2020-09-21T20:00:00")
+                },
+                new TemperatureSensorModel
+                {
+                    Temperature = 77,
+                    Humidity = 0.22,
+                    TimeCaptured = DateTime.Parse("2020-09-21T20:00:00")
+                },
+                new TemperatureSensorModel
+                {
+                    Temperature = 77,
+                    Humidity = 0.22,
+                    TimeCaptured = DateTime.Parse("2020-09-21T20:00:00")
+                }
+            };
+            return output;
+        }
+    }
+
+}
--- WeatherStationAPI.UnitTest/UnitTest1.cs
+++ WeatherStationAPI.UnitTest/UnitTest1.cs
@@ -8,42 +8,42 @@ namespace WeatherStationAPI.UnitTest
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
-            DateTime start;
-            DateTime end;
-            using (var client = new HttpClient())
-            {
-                start = DateTime.Now;
-                var response = client.GetAsync("https://localhost:44383/api/WindData/GetAllData");
-                end = DateTime.Now;
-            }
-            var expected = 1;
-            var actual = (int)(end - start).TotalMilliseconds;
-            Assert.True(actual <= expected, $"Expected total milliseconds of less than or equal to {expected} but was {actual}.");
-        }
+        //[Fact]
+        //public void Test1()
+        //{
+        //    DateTime start;
+        //    DateTime end;
+        //    using (var client = new HttpClient())
+        //    {
+        //        start = DateTime.Now;
+        //        var response = client.GetAsync("https://localhost:44383/api/WindData/GetAllData");
+        //        end = DateTime.Now;
+        //    }
+        //    var expected = 1;
+        //    var actual = (int)(end - start).TotalMilliseconds;
+        //    Assert.True(actual <= expected, $"Expected total milliseconds of less than or equal to {expected} but was {actual}.");
+        //}
 
-        [Fact]
-        public void AverageResponseTime_100Requests()
-        {
-            var allResponseTimes = new List<(DateTime Start, DateTime End)>();
+        //[Fact]
+        //public void AverageResponseTime_100Requests()
+        //{
+        //    var allResponseTimes = new List<(DateTime Start, DateTime End)>();
 
-            for (var i = 0; i < 100; i++)
-            {
-                using (var client = new HttpClient())
-                {
-                    var start = DateTime.Now;
-                    var response = client.GetAsync("https://localhost:44383/api/WindData/GetAllData").Result;
-                    var end = DateTime.Now;
+        //    for (var i = 0; i < 100; i++)
+        //    {
+        //        using (var client = new HttpClient())
+        //        {
+        //            var start = DateTime.Now;
+        //            var response = client.GetAsync("https://localhost:44383/api/WindData/GetAllData").Result;
+        //            var end = DateTime.Now;
 
-                    allResponseTimes.Add((start, end));
-                }
-            }
+        //            allResponseTimes.Add((start, end));
+        //        }
+        //    }
 
-            var expected = 1;
-            var actual = (int)allResponseTimes.Select(r => (r.End - r.Start).TotalMilliseconds).Average();
-            Assert.True(actual <= expected, $"Expected average response time of less than or equal to {expected} ms but was {actual} ms.");
-        }
+        //    var expected = 1;
+        //    var actual = (int)allResponseTimes.Select(r => (r.End - r.Start).TotalMilliseconds).Average();
+        //    Assert.True(actual <= expected, $"Expected average response time of less than or equal to {expected} ms but was {actual} ms.");
+        //}
     }
 }
